f855f753883c68ddb8d7eecb4c710ca27692cf55
Enmanuel Medrano <enmanuel_medrano@hotmail.com>
2019-11-17 13:52:35 -0400

    Working with function on SandBox

--- sandbox/js-basics/index.js
+++ sandbox/js-basics/index.js
@@ -11,11 +11,27 @@ const itAlive=true;
 // let lastName='';
 // let age=0;
 // let birthdayDate=new Date();
+let selection='all';
+let colorsList=['red','black'];
 let person ={
      firstName:null
     ,lastName:null
     ,age:0
+    ,favoriteColor:null
     ,birthdayDate:new Date()
+}
+
+function showOnBrowser() {
+    
+}
+
+function clearPerson() {
+    person=null 
+}
+
+function getAge(inDate) {
+    //inDate =new Date(inDate);
+    return Math.floor((CURRENT_DATE-inDate)/(MS_PER_YEAR));
 }
 
 // Dot Notation
@@ -25,19 +41,21 @@ let person ={
 // person.age=Math.floor((CURRENT_DATE-person.birthdayDate.getTime())/(MS_PER_YEAR));
 
 //Bracket Notation
-person['birthdayDate'].setFullYear(1994,2,03);
 person['firstName'] = 'Enmanuel';
 person['lastName'] = 'Medrano';
-person['age']=Math.floor((CURRENT_DATE-person.birthdayDate.getTime())/(MS_PER_YEAR));
+person['birthdayDate'].setFullYear(1994,2,03);
+person['favoriteColor']='blue';
+person['age']=getAge(person['birthdayDate']);
+
+console.log(getAge(person['birthdayDate']));
+colorsList[2]=person.favoriteColor;
 
 console.log('Name|'+person.firstName + ' ' + person.lastName);
 console.log('Age| '+person.age.toString());
 console.log('Birthday| '+person.birthdayDate.toString());
+//console.log(colorsList.length);
+clearPerson();
 
-person.firstName =  null;
-person.lastName =  null;
-person.birthdayDate = null;
-person.ge=null;
 
 
 
