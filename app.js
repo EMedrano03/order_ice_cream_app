66ef67134c0f8d6cbdb752c4de69f5475ebfb962
Enmanuel Medrano <enmanuel_medrano@hotmail.com>
2019-11-18 05:52:52 -0400

    configurando app y server js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+const orderRoutes = require("./api/routes/orders");
+const userRoutes = require('./api/routes/user');
+
+app.use(morgan('dev'));
+app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+  });
+  
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+  });
+  
+  module.exports = app;
