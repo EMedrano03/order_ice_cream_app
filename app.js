2648fc4cc4d4969ef53628154dfd9cc6a28c1da7
Enmanuel Medrano <enmanuel_medrano@hotmail.com>
2019-11-18 07:02:12 -0400

    Trabajando con los request de las ordenes

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const orderRoutes = require("./api/routes/orders");
 //const userRoutes = require('./api/routes/user');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
+
 app.use("/orders", orderRoutes);
 //app.use("/user", userRoutes);
 
